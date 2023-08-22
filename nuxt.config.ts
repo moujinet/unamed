8d829caa4ff5c8ca88c3a932545af93e6bfc1106
羽白 <luoyi@mouji.net>
2023-08-22 11:54:01 +0800

    feat: first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,125 @@
+import { isDevelopment } from 'std-env'
+
+export default defineNuxtConfig({
+  app: {
+    keepalive: true,
+    rootId: 'app',
+    head: {
+      viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        {
+          name: 'description',
+          content: 'A lightweight, self-hosted bookmarking web service.',
+        },
+        {
+          name: 'apple-mobile-web-app-status-bar-style',
+          content: 'black-translucent',
+        },
+        { property: 'og:title', content: 'unamed' },
+        {
+          property: 'og:description',
+          content: 'A lightweight, self-hosted bookmarking web service.',
+        },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: '/unamed-og.png' },
+        { property: 'og:image:width', content: '3800' },
+        { property: 'og:image:height', content: '1900' },
+        { property: 'og:site_name', content: 'unamed' },
+        { property: 'twitter:site', content: '@moujinet' },
+        { property: 'twitter:title', content: 'unamed' },
+        { property: 'twitter:card', content: 'summary_large_image' },
+      ],
+    },
+  },
+
+  modules: [
+    '@unocss/nuxt',
+    '@nuxtjs/color-mode',
+    '@vueuse/nuxt',
+    'nuxt-icon',
+    '@nuxt/image',
+    '@morev/vue-transitions/nuxt',
+    'stale-dep/nuxt',
+  ],
+
+  vue: {
+    defineModel: true,
+  },
+
+  css: [
+    '@unocss/reset/tailwind.css',
+    'floating-vue/dist/style.css',
+    '~/styles/vars.css',
+    '~/styles/global.css',
+    '~/styles/scrollbar.css',
+    '~/styles/popper.css',
+  ],
+
+  postcss: {
+    plugins: {
+      'postcss-nested': {},
+    },
+  },
+
+  colorMode: { classSuffix: '' },
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: true,
+    },
+    publicAssets: [
+      {
+        dir: '~/public/fonts',
+        maxAge: 24 * 60 * 60 * 365, // 1 year (versioned)
+        baseURL: '/fonts',
+      },
+      {
+        dir: '~/public/upload',
+        maxAge: 24 * 60 * 60 * 365, // 1 year (versioned)
+        baseURL: '/upload',
+      },
+    ],
+  },
+
+  image: {
+    presets: {
+      avatar: {
+        modifiers: {
+          fit: 'cover',
+          format: 'png',
+          width: 42,
+          height: 42,
+        },
+      },
+      favicon: {
+        modifiers: {
+          fit: 'cover',
+          format: 'png',
+          width: 42,
+          height: 42,
+        },
+      },
+    },
+  },
+
+  staleDep: {
+    packageManager: 'pnpm',
+  },
+
+  devtools: { enabled: isDevelopment },
+
+  sourcemap: isDevelopment,
+})
---
+++ unocss.config.ts
@@ -0,0 +1,71 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+import presetRemToPx from '@unocss/preset-rem-to-px'
+
+export default defineConfig({
+  theme: {
+    colors: {
+      primary: 'rgba(var(--c-primary), <alpha-value>)',
+      danger: 'rgba(var(--c-danger), <alpha-value>)',
+    },
+  },
+  shortcuts: [
+    {
+      // background
+      'bg-main': 'bg-$c-bg-base',
+      'bg-sider': 'bg-$c-bg-sider',
+      'bg-inset': 'bg-$c-bg-inset',
+      'bg-inset-hover': 'bg-$c-bg-inset-hover',
+      'bg-inset-active': 'bg-$c-bg-inset-active',
+
+      // text
+      'text-base': 'text-$c-text-base',
+      'text-caption': 'text-$c-text-caption',
+      'text-comment': 'text-$c-text-comment',
+      'text-light': 'text-$c-text-light',
+
+      // border
+      'border-base': 'border-$c-border-base',
+      'border-hover': 'border-$c-border-hover',
+      'border-active': 'border-$c-border-active',
+
+      // utils
+      'flex-center': 'items-center justify-center',
+      'flex-v-center': 'items-center',
+      'flex-h-center': 'justify-center',
+      'absolute-center': 'left-50% top-50% translate-x--1/2 translate-y--1/2',
+      'absolute-v-center': 'top-50% translate-y--1/2',
+      'absolute-h-center': 'left-50% translate-x--1/2',
+    },
+  ],
+  presets: [
+    presetUno({
+      attributifyPseudo: true,
+    }),
+    presetAttributify(),
+    presetRemToPx(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+        'line-height': '1',
+      },
+    }),
+    presetTypography(),
+    presetWebFonts({
+      provider: 'none',
+      fonts: {
+        sans: 'Inter',
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+})
