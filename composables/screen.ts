bab45c0202f35b667bbb7b58c654ce734c2fda6f
羽白 <luoyi@mouji.net>
2023-08-22 13:21:08 +0800

    feat(composable): update

---
+++ composables/screen.ts
@@ -0,0 +1,6 @@
+import { breakpointsTailwind } from '@vueuse/core'
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+export const isMobile = breakpoints.smaller('sm')
+export const isSmaller = breakpoints.smaller('md')
