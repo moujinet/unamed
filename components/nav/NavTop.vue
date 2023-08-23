061750d7e2147dbf55722d8fb79e95acd6abba29
羽白 <luoyi@mouji.net>
2023-08-23 14:01:31 +0800

    fix(component): append NavTop to BODY

--- components/nav/NavTop.vue
+++ components/nav/NavTop.vue
@@ -8,11 +8,13 @@ function goTop() {
 </script>
 
 <template>
-  <div
-    :class="y > height / 2 ? 'flex-(~ center)' : 'hidden'"
-    class="fixed bottom-20 right-4 z-200 h-14 w-14 rounded-full bg-sider op-70 sm:bottom-4 active:op-100"
-    @click="goTop()"
-  >
-    <CommonIcon name="i-ph-rocket" size="24" class="text-primary" />
-  </div>
+  <Teleport to="body">
+    <div
+      :class="y > height / 2 ? 'flex-(~ center)' : 'hidden'"
+      class="fixed bottom-20 right-4 z-200 h-14 w-14 rounded-full bg-sider op-70 sm:bottom-4 active:op-100"
+      @click="goTop()"
+    >
+      <CommonIcon name="i-ph-rocket" size="24" class="text-primary" />
+    </div>
+  </Teleport>
 </template>
