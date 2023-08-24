c69e8355169c854e97248a5a1f12be5e54c32a5e
羽白 <luoyi@mouji.net>
2023-08-24 09:37:16 +0800

    fix(component): ContentSplitView Hydrated

--- components/content/ContentSplitView.vue
+++ components/content/ContentSplitView.vue
@@ -5,15 +5,15 @@ const { showNav, showContent } = useSplitView('settings')
 <template>
   <div h-full flex transition-all>
     <div
-      v-if="showNav"
-      :class="isMobile ? 'w-full' : 'w-[calc(100%-68px)] md:w-300px border-(r base)'"
+      v-show="isHydrated && showNav"
+      :class="isMobile ? 'w-full' : 'w-[calc(100%-68px)] md:w-280px border-(r base)'"
       fixed top-0 z-10 h-full bg-main
     >
       <slot />
     </div>
     <div
       v-show="isHydrated && showContent"
-      :class="[showNav ? 'md:ml-300px md:w-[calc(100%-300px)]' : 'w-full']"
+      class="w-full md:ml-280px md:w-[calc(100%-280px)]"
       bg-main
     >
       <NuxtPage />
