5b5e7ccba24a118c24b8479f9505f064345f9270
羽白 <luoyi@mouji.net>
2023-08-29 09:36:15 +0800

    chore: remove unused file

--- composables/collection.ts
+++
@@ -1,8 +0,0 @@
-import type { ICollectionIcon } from '~/types'
-
-export async function useCollectionIcons() {
-  return await useAsyncData(
-    'collection-icons',
-    () => $fetch<ICollectionIcon[]>('/json/collection-icons.json'),
-  )
-}
