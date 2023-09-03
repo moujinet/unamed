ca62ceb439604c5ccb953604789351a81b3e3e90
羽白 <luoyi@mouji.net>
2023-09-03 21:09:17 +0800

    chore(page): remove unused pages

--- pages/user/collections.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-</script>
-
-<template>
-  <ContentView title="Collections" icon="i-ph-stack">
-    Collections
-  </ContentView>
-</template>
--- pages/user/tags.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-</script>
-
-<template>
-  <ContentView title="Tags" icon="i-ph-hash">
-    Tags
-  </ContentView>
-</template>
